d3214cfedd8655b5898a6e1eda4a507299ed3dbc
Zainab Yaqub <30675460+ayyyzainab@users.noreply.github.com>
2023-04-27 14:17:46 -0400

    Add files via upload

--- stockticker.js
+++ stockticker.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://nosqlpractice.gfa9bh0.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+const url = "mongodb+srv://nosqlpractice.gfa9bh0.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl=true&sslValidate=false"
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/companies');
 const mongoose = require('mongoose');
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(url);
 
   if(err) { return console.log(err); return;}
   var dbo = db.db("library");
@@ -26,5 +26,3 @@ async function main() {
 
     db.close();
   }
-
-
