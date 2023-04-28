dc43a0d8430a83ac69278f4f53737f119ac40b63
Zainab Yaqub <30675460+ayyyzainab@users.noreply.github.com>
2023-04-28 13:46:11 -0400

    Update stock1.js

--- stock1.js
+++ stock1.js
@@ -1,26 +1,63 @@
+const {MongoClient} = require('mongodb');
+const http = require('http');
+const fs = require('fs');
+const { parse } = require("csv-parse");
+const formidable = require('formidable');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://zainabyaqub:fO2htkZRkZuSUfLQ@nosqlpractice.gfa9bh0.mongodb.net/?retryWrites=true&w=majority";
+async function main(filepath) {
+	console.log("Made it to main function");
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+    const url = process.env.MONGODB_URI;
+    const client = new MongoClient(url);
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        var database = client.db("companies");
+        var collections = database.collection("companies");
+        var stocks = [];
+
+        fs.createReadStream(filepath)
+          .pipe(parse({ delimiter: ",", from_line: 2 }))
+          .on('data', function (row) {
+            var stock = { name: row[0], ticker: row[1], price: row[2] };
+            stocks.push(stock);
+          })
+          .on("end", function() {
+            collections.insertMany(stocks);
+            client.close();
+          });
+    } catch (e) {
+        console.error(e);
+    }
 }
-run().catch(console.dir);
+	
+http.createServer(function (req, res) {
+	  
+    if (req.url == "/process") {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            if (Object.keys(files).length === 0) {
+                res.write("Error: No file uploaded");
+                return res.end();
+            }
+            var tempFilePath = files.filetoupload.filepath;
+            var projectFilePath = __dirname + '/uploads/' + files.filetoupload.originalFilename;
+            fs.rename(tempFilePath, projectFilePath, function (err) {
+                if (err) throw err;
+                res.write('File has been uploaded!');
+                main(projectFilePath);
+                res.end();
+            });
+        });
+    }
+    else {
+        fs.readFile('index.html', (err, html) => {
+            if (err) {
+                throw err;
+            }
+            res.setHeader('Content-type', 'text/html');
+            res.write(html);
+            res.statusCode = 200;
+            res.end();
+        });
+    }
+}).listen(process.env.PORT || 8080);
